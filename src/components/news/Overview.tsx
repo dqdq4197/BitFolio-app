59d25c2b786725a4c64659ada61af2f81ab4b305
choiheesu <dqdq4197@naver.com>
2021-09-26 04:34:16 +0900

    Add News Feeds infinte scroll & modify ScrollView -> FlatList

--- src/components/news/Overview.tsx
+++ src/components/news/Overview.tsx
@@ -1,72 +1,102 @@
 import React, { useState, useCallback } from 'react';
-import { Animated, LayoutAnimation, Dimensions, ActivityIndicator } from 'react-native';
+import { 
+  Animated, 
+  LayoutAnimation, 
+  ActivityIndicator,
+  Platform,
+  UIManager,
+  FlatList
+} from 'react-native';
 import styled from 'styled-components/native';
 import { useTranslation } from 'react-i18next';
+import * as Haptics from 'expo-haptics';
 import useNewsArticles from '/hooks/useNewsArticles';
+import useGlobalTheme from '/hooks/useGlobalTheme';
 import { useAppSelector, useAppDispatch } from '/hooks/useRedux';
 import useAnimatedHeaderTitle from '/hooks/useAnimatedHeaderTitle';
 import { NewsStateType, changeSortOrder, SortOrderType } from '/store/news';
 import { NewsData } from '/lib/api/CryptoCompareReturnType'; 
-import ScrollView from '/components/common/ScrollView';
 import Text from '/components/common/Text';
 import CustomRefreshControl from '/components/common/CustomRefreshControl';
+import ItemSkeleton from '/components/skeletonPlaceholder/news/Item';
+import SurfaceTopView from '/components/common/SurfaceTopView';
 import Item from './Item';
 import FiltersBar from './FiltersBar';
-import ItemSkeleton from '../skeletonPlaceholder/news/Item';
 
-const { width, height } = Dimensions.get('window');
+if (Platform.OS === 'android') {
+  UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
-interface TitleProps extends Pick<NewsStateType, 'sortOrder'> {}
+interface TitleProps extends Pick<NewsStateType, 'sortOrder'> {
+  scrollY: Animated.Value
+  onResetLTs: () => void
+}
 
-const Title = ({ sortOrder }: TitleProps) => {
+const Title = ({ sortOrder, scrollY, onResetLTs }: TitleProps) => {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
 
   const handleSortOrderChange = (order: SortOrderType) => {
+    Haptics.impactAsync();
+    onResetLTs();
     dispatch(changeSortOrder(order));
   }
 
   return (
-    <TitleWrap>
-      <Text
-        fontXL 
-        color100 
-        heavy 
-        margin="0 15px 0 0"
-      >
-        { sortOrder === 'latest' 
-          ? t(`news.latest`)
-          : t(`news.popular`)
-        }
-      </Text>
-      <Text 
-        onPress={() => handleSortOrderChange(sortOrder === 'latest' ? 'popular' : 'latest')}
-        fontL 
-        heavy
-      >
-        { sortOrder === 'latest' 
-          ? t(`news.popular`)
-          : t(`news.latest`)
-        }
-      </Text>
+    <TitleWrap
+      as={Animated.View}
+      style={{
+        height: scrollY.interpolate({
+          inputRange: [0, 100],
+          outputRange: [40, 0],
+          extrapolate: 'clamp'
+        })
+      }}
+    >
+      <TitleTextWrap>
+        <Text
+          fontXL 
+          color100 
+          heavy 
+          margin="0 15px 0 0"
+        >
+          { sortOrder === 'latest' 
+            ? t(`news.latest`)
+            : t(`news.popular`)
+          }
+        </Text>
+        <Text 
+          onPress={() => handleSortOrderChange(sortOrder === 'latest' ? 'popular' : 'latest')}
+          fontL 
+          heavy
+        >
+          { sortOrder === 'latest' 
+            ? t(`news.popular`)
+            : t(`news.latest`)
+          }
+        </Text>
+      </TitleTextWrap>
     </TitleWrap>
   )
 }
 const Overview = () => {
   const { t } = useTranslation();
+  const { theme } = useGlobalTheme();
   const { categories, feeds, sortOrder } = useAppSelector(state => state.newsReducer);
   const { scrollY } = useAnimatedHeaderTitle({ 
     title: t(`news.${sortOrder}`) + ' ' + t(`common.news`), 
-    triggerPoint: 30 
+    triggerPoint: 60 
   });
+  const [newsData, setNewsData] = useState<NewsData[] | null>(null);
+  const [refreshing, setRefreshing] = useState(false);  
+  const [lTs, setLTs] = useState<number | undefined>(undefined);
   const { data, mutate, isValidating } = useNewsArticles({ 
+    lTs,
     categories,
     sortOrder,
     feeds,
     suspense: false
   });
-  const [newsData, setNewsData] = useState<NewsData[] | null>(null);
-  const [refreshing, setRefreshing] = useState(false);  
 
   React.useEffect(() => {
     if(data) {
@@ -77,7 +107,15 @@ const Overview = () => {
           LayoutAnimation.Properties.opacity
         )
       );
-      setNewsData(data[0].Data);
+        
+      if(lTs === undefined || !newsData) {
+        setNewsData(data[0].Data);
+      } else {
+        // infinite loading ...
+        if(newsData.slice(-1)[0].published_on === data[0].Data.slice(-1)[0].published_on) return ;
+
+        setNewsData(prevState => prevState ? [...prevState, ...data[0].Data] : null);
+      }
     }
   }, [data])
 
@@ -88,58 +126,84 @@ const Overview = () => {
 
   const handleRefresh = useCallback(async() => {
     setRefreshing(true);
-    await mutate()
+    if(lTs === undefined) {
+      await mutate()
+    } else {
+      setLTs(undefined);
+    }
     setRefreshing(false);
-  }, []);
+  }, [refreshing, lTs]);
+
+  const handleEndReached = () => {
+    if(!newsData || sortOrder === 'popular') return ;
+    
+    setLTs(newsData.slice(-1)[0].published_on);
+  }
+  
+  const onResetLTs = () => {
+    setLTs(undefined);
+  }
 
   return (
     <>
-      { isValidating && (
+      { isValidating && newsData && (
         <IndicatorWrap>
           <ActivityIndicator 
             size="large"
           />
         </IndicatorWrap>
       ) }
-      <Container
-        as={ Animated.ScrollView }
+      <Title 
+        sortOrder={sortOrder} 
+        scrollY={scrollY}
+        onResetLTs={onResetLTs}
+      />
+      <FlatList 
+        data={newsData}
         onScroll={ handleScroll }
-        stickyHeaderIndices={[1]}
+        ListHeaderComponent={
+          <>
+            <SurfaceTopView />
+            <FiltersBar />
+          </>
+        }
+        stickyHeaderIndices={[0]}
+        contentContainerStyle={{ backgroundColor: theme.base.background.surface }}
         refreshControl={
           <CustomRefreshControl
             onRefresh={handleRefresh}
             refreshing={refreshing}
           />
         }
-      >
-        <Title sortOrder={sortOrder}/>
-        <FiltersBar />
-        { newsData 
-          ? newsData.map(item => {
-              return (
-                <Item 
-                  key={item.id}
-                  item={item}
-                  currentCategory={null}
-                />
-              )
-            })
-          : <ItemSkeleton />
+        ListEmptyComponent={ <ItemSkeleton /> }
+        renderItem={
+          ({ item }) => {
+            return (
+              <Item 
+                key={item.id}
+                item={item}
+                currentCategory={null}
+              />
+            )
+          }
         }
-      </Container>
+        onEndReached={handleEndReached}
+        onEndReachedThreshold={0.5}
+      />
     </>
   )
 }
 
 export default Overview;
 
-const Container = styled(ScrollView)`
+const TitleWrap = styled.View`
+  padding: ${({ theme }) => `0 ${theme.content.spacing}`};
   background-color: ${({ theme }) => theme.base.background.surface};
 `
-const TitleWrap = styled.View`
+
+const TitleTextWrap = styled.View`
   flex-direction: row;
   align-items: flex-end;
-  padding: ${({ theme }) => `${theme.content.blankSpacing} ${theme.content.spacing}`};
 `
 
 const IndicatorWrap = styled.View`
