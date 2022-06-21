735c50ad75aa681934e27cd373d09fa26b155685
choiheesu <dqdq4197@naver.com>
2022-06-21 16:22:16 +0900

    Add Email Verification

--- src/hooks/context/useAuthContext.tsx
+++ src/hooks/context/useAuthContext.tsx
@@ -5,13 +5,20 @@ import React, {
   useContext,
   useMemo,
 } from 'react';
-import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import {
+  getAuth,
+  onAuthStateChanged,
+  User,
+  signOut,
+  Auth,
+} from 'firebase/auth';
 
 const AuthContext = createContext<ValueType | undefined>(undefined);
 
 type ValueType = {
   isLoading: boolean;
   currentUser: User | null;
+  auth: Auth;
 };
 
 type ProviderProps = {
@@ -24,7 +31,33 @@ export function AuthProvider({ children }: ProviderProps) {
   const auth = getAuth();
 
   useEffect(() => {
+    if (auth && !auth.currentUser?.emailVerified) {
+      signOut(auth);
+      console.log('초기 로드 & 로그 아웃');
+    }
+  }, [auth]);
+
+  /**
+   * 로그인 시.
+   * 이메일 인증되어 있으면 정상적으로 홈 리다이렉트
+   * 이메일 인증 안되어 있으면 이메일 인증 페이지로 이동
+   * 회원 가입 시.
+   * 회원 가입하고 이메일 인증 페이지로 이동
+   *
+   * 공통.
+   * 이메일 인증 페이지에서 나가면 sign out
+   * 최초 앱 실행시.
+   * 이메일 인증 여부 확인하고, 인증 했으면 정상 작동
+   * 안했으면 sign out
+   *
+   * 이메일 인증 완료하면 user.reload() 실행
+   */
+  useEffect(() => {
     const unSubscribeAuth = onAuthStateChanged(auth, user => {
+      if (user) {
+        setCurrentUser(user);
+      }
+
       if (user) {
         setCurrentUser(user);
       } else {
@@ -39,10 +72,11 @@ export function AuthProvider({ children }: ProviderProps) {
 
   const initialData = useMemo(
     () => ({
+      auth,
       currentUser,
       isLoading,
     }),
-    [currentUser, isLoading]
+    [auth, currentUser, isLoading]
   );
 
   return (
---
+++ src/hooks/firebase/useSendEmailVerification.tsx
@@ -0,0 +1,73 @@
+import { useState, useCallback, useMemo, useEffect } from 'react';
+import { sendEmailVerification as firebaseSendEmailVerification } from 'firebase/auth';
+import { useTranslation } from 'react-i18next';
+import { FirebaseError } from '@firebase/util';
+import { FIREBASE_EMAIL_VERIFICATION_DYNAMICLINK, APP_BUNDLE_ID } from '@env';
+
+import { useAuthContext } from '/hooks/context/useAuthContext';
+
+const useSendEmailVerification = () => {
+  const { t } = useTranslation();
+  const { currentUser } = useAuthContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [verified, setVerified] = useState(false);
+
+  // 1초마다 인증 완료 여부 체크.
+  useEffect(() => {
+    let interval: NodeJS.Timer;
+    if (currentUser) {
+      setVerified(currentUser.emailVerified);
+      interval = setInterval(async () => {
+        await currentUser.reload();
+        if (currentUser.emailVerified) {
+          setVerified(true);
+          clearInterval(interval);
+        }
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [currentUser]);
+
+  const sendEmailVerification = useCallback(async () => {
+    if (!currentUser || currentUser.emailVerified) return;
+    setIsLoading(true);
+    setErrorMessage(undefined);
+    try {
+      await firebaseSendEmailVerification(currentUser, {
+        url: FIREBASE_EMAIL_VERIFICATION_DYNAMICLINK,
+        iOS: {
+          bundleId: APP_BUNDLE_ID,
+        },
+      });
+    } catch (error) {
+      let message = t(`auth.errorMessage.unkown-error`);
+      if (error instanceof FirebaseError) {
+        const { code } = error;
+
+        switch (code) {
+          case 'auth/too-many-requests':
+            message = t(`auth.errorMessage.too-many-requests`);
+            break;
+          default:
+            break;
+        }
+      }
+      setErrorMessage(message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [currentUser, t]);
+
+  return useMemo(
+    () => ({
+      sendEmailVerification,
+      errorMessage,
+      isLoading,
+      verified,
+    }),
+    [errorMessage, isLoading, sendEmailVerification, verified]
+  );
+};
+
+export default useSendEmailVerification;
