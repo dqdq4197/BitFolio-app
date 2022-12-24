adc6ade0ba6edf245c0d8c69712a71cad6f84024
choiheesu <dqdq4197@naver.com>
2022-12-25 00:48:02 +0900

    Modify speed of watchlist icon animation

--- src/components/common/WatchListIcon.tsx
+++ src/components/common/WatchListIcon.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef, useMemo, useState } from 'react';
-import styled from 'styled-components/native';
-import * as Haptics from 'expo-haptics';
 import LottieView from 'lottie-react-native';
+import React, { useCallback, useEffect, useMemo, useRef } from 'react';
+import styled from 'styled-components/native';
 
+import useFirstMountState from '/hooks/useFirstMountState';
 import { useAppDispatch, useAppSelector } from '/hooks/useRedux';
 import { changeWatchList } from '/store/slices/baseSetting';
 
@@ -12,46 +12,51 @@ type IconProps = {
 
 const WatchListIcon = ({ id }: IconProps) => {
   const { watchList } = useAppSelector(state => state.baseSettingReducer);
-  const [isRendered, setIsRendered] = useState(false);
   const animationRef = useRef<LottieView>(null);
+  const isFirstMount = useFirstMountState();
+
   const dispatch = useAppDispatch();
 
-  const handleHeartPress = () => {
+  const handleHeartPress = useCallback(() => {
     dispatch(changeWatchList(id));
-    Haptics.impactAsync();
-    if (isActive) {
-      animationRef.current?.reset();
-    } else {
-      animationRef.current?.play(20, 140);
-    }
-  };
+  }, [id, dispatch]);
 
   const isActive = useMemo(() => {
     return watchList.includes(id);
   }, [id, watchList]);
 
   useEffect(() => {
-    if (!isRendered) {
-      setIsRendered(true);
+    if (!animationRef.current) {
+      return;
+    }
+
+    if (isFirstMount && isActive) {
+      animationRef.current.play(130, 130);
+    }
+
+    if (!isFirstMount) {
       if (isActive) {
-        animationRef.current?.play(130, 130);
+        animationRef.current.play(20, 140);
+      } else {
+        animationRef.current.reset();
       }
     }
-  }, [isActive, isRendered]);
+  }, [isActive, isFirstMount]);
 
   return (
     <Container onPress={handleHeartPress}>
       <LottieView
         ref={animationRef}
         source={require('../../../assets/lottie/watchListIcon.json')}
+        speed={2}
         style={{
-          width: 56,
-          height: 56,
+          width: 62,
+          height: 62,
           transform: [
             {
-              translateX: -6,
+              translateX: -8,
             },
-            { translateY: -6 },
+            { translateY: -7 },
           ],
         }}
         loop={false}
