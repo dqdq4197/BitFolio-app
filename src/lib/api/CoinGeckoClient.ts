25d857df57eb76f9502774bc8aaa9264f1350216
choiheesu <dqdq4197@naver.com>
2025-05-06 19:56:56 +0900

    refactor: remove legacy search API #49

--- src/lib/api/CoinGeckoClient.ts
+++ src/lib/api/CoinGeckoClient.ts
@@ -56,10 +56,6 @@ type MarketChartRangeParams = {
   to: number // UNIX TimeStamp
 }
 
-type SearchParams = {
-  locale: baseTypes.Language
-}
-
 type HistoricalOhlcParams = {
   vs_currency: baseTypes.Currency
   days: ChartTimeIntervalType // Data up to number of days ago (1/7/14/30/90/180/365/max)
@@ -211,12 +207,6 @@ export const CoinGecko = {
     DetailInfo: (id: string, params: DetailInfoParams) => {
       return {
         url: `/coins/${id}`,
-        params,
-      }
-    },
-    search: (params: SearchParams) => {
-      return {
-        url: `/search`,
         params,
       }
     },
